8f5ac2f5ebf3409aa33a209eecb37e2decaba542
dennep209-pixel <dennep209@gmail.com>
2026-09-17 14:33:35 +0300

    вже є таблиця

--- lab1_part2/main.c
+++ lab1_part2/main.c
@@ -9,7 +9,7 @@ int main()
     int N;
     int n;
 
-    double start, end, delta, x;
+    double start, end, delta, x, y;
 
     // виконуємо введення змінної variant
     // При введені недопустимих значень відбувається повторне введення
@@ -34,13 +34,35 @@ int main()
     {
          printf("Enter N: ");
         scanf("%d", &N);
+        delta = (end - start) / (N - 1);
     }
     else if (variant == 2)
     {
         printf("Enter delta: ");
         scanf("%lf", &delta);
+        N = (int)((end - start) / delta) + 1;
     }
 
+    printf("X1 = %.2lf\n", start);
+    printf("X2 = %.2lf\n", end);
+    printf("N = %d\n", N);
+    printf("delta = %.2lf\n", delta);
+
+    printf("+-------+---------------------+---------------------+\n");
+printf("| Point |          X          |        F(X)         |\n");
+printf("+-------+---------------------+---------------------+\n");
+
+    for (n = 0; n < N; n++)
+    {
+    x = start + n * delta;
+
+    y = 0.25 * (x - 25) * (x - 25)
+        + ((x + 25) * (x + 25) * (x + 25)) / 100
+        + 1;
+
+    printf("|%7d|%21.2lf|%21.2lf|\n", n + 1, x, y);
+}
+    printf("+-------+---------------------+---------------------+\n");
     printf("Program finished\n");
 
     return 0;
